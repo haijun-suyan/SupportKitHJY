55f91ed20c5674a078621c9dba2c313a15e5cd72
yanhaijun <15261882407@163.com>
2024-04-21 15:52:12 +0800

    Type:ReqDev Desc:二方库维护 Version:

---
+++ SupportTool.h
@@ -0,0 +1,14 @@
+//
+//  SupportTool.h
+//  SupportKitHJY
+//
+//  Created by haijunyan on 2024/4/19.
+//
+
+#import <Foundation/Foundation.h>
+#import <BaseKitHJY/UnitProtocol.h>
+
+@interface SupportTool : NSObject<UnitProtocol>
+
+@end
+
---
+++ SupportTool.m
@@ -0,0 +1,20 @@
+//
+//  SupportTool.m
+//  SupportKitHJY
+//
+//  Created by haijunyan on 2024/4/19.
+//
+
+#import "SupportTool.h"
+
+
+@interface SupportTool()
+
+@end
+
+
+@implementation SupportTool
+- (void)phoneCall{
+    NSLog(@"phoneCallphoneCallphoneCallphoneCall");
+}
+@end
