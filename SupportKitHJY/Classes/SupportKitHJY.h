20b6c3b28c17659b2c725e77b921b53262fbdbc5
yanhaijun <15261882407@163.com>
2024-04-21 17:40:59 +0800

    Type:ReqDev Desc:二方库维护 Version:

---
+++ SupportKitHJY/Classes/SupportKitHJY.h
@@ -0,0 +1,13 @@
+//
+//  SupportKitHJY.h
+//  SupportKitHJY
+//
+//  Created by haijunyan on 2024/4/21.
+//
+
+#ifndef SupportKitHJY_h
+#define SupportKitHJY_h
+//实现供业务项目层的间接(具体)访问
+
+
+#endif /* SupportKitHJY_h */
--- SupportKitHJY/Classes/SupportTool.h
+++ SupportKitHJY/Classes/SupportTool.h
@@ -6,7 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <BaseKitHJY/UnitProtocol.h>
+#import <BaseKitHJY/BaseKitHJY.h>
 
 @interface SupportTool : NSObject<UnitProtocol>
 
